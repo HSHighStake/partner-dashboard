2db0f36875e06aed0cf13312cba47943a4378e95
Roman <romka_gorev@mail.ru>
2018-04-11 19:49:51 +0200

    hs-salary-formular component updated

--- src/app/hs-salary-formular/hs-salary-formular.component.ts
+++ src/app/hs-salary-formular/hs-salary-formular.component.ts
@@ -9,6 +9,15 @@ import { FormsModule } from '@angular/forms';
 export class HsSalaryFormularComponent {
   technologies;
   types;
+  levels;
+  payouts;
+  working_time_types;
+  reserves;
+  holidays;
+
+  setHolidays(event: any) {
+    this.holidays = event.target.value;
+  }
 
   // @todo #3 This should developed as a small service.
   // @todo #3 Whats the problem!
@@ -37,5 +46,66 @@ export class HsSalaryFormularComponent {
         id: 'java'
       }
     ];
+    this.levels = [
+      {
+        label: 'Junior',
+        id: 'junior'
+      },
+      {
+        label: 'Middle',
+        id: 'middle'
+      },
+      {
+        label: 'Senior',
+        id: 'senior'
+      }
+    ];
+    this.payouts = [
+      {
+        label: 'Weekly',
+        id: 'weekly'
+      },
+      {
+        label: 'Monthly',
+        id: 'monthly'
+      },
+      {
+        label: 'Quarterly',
+        id: 'quarterly'
+      },
+      {
+        label: 'Yearly',
+        id: 'yearly'
+      }
+    ];
+    this.working_time_types = [
+      {
+        label: '4 days x 8 hours',
+        id: '4_days_8_hours'
+      },
+      {
+        label: '4 days x 10 hours',
+        id: '4_days_10_hours'
+      },
+      {
+        label: '5 days x 8 hours',
+        id: '5_days_8_hours'
+      },
+      {
+        label: '5 days x 6 hours',
+        id: '5_days_6_hours'
+      }
+    ];
+    this.reserves = [
+      {
+        label: 'Reserves for holiday pay',
+        id: 'holiday_pay'
+      },
+      {
+        label: 'Reserves for Christmas pay',
+        id: 'christmas_pay'
+      },
+    ];
+    this.holidays = 24;
   }
 }
