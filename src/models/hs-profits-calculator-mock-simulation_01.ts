e77bf12aa602901afc3e0f942de92a49a2c90f04
nitr0n <nitr0n@gmx.de>
2018-04-11 22:13:46 +0200

    Added antoher example

---
+++ src/models/hs-profits-calculator-mock-simulation_01.ts
@@ -0,0 +1,96 @@
+export const data = {
+    "dayFacts": [
+        {
+            "category": "Working",
+            "count": 217
+        },
+        {
+            "category": "Weekend",
+            "count": 104
+        },
+        {
+            "category": "Holiday",
+            "count": 14
+        },
+        {
+            "category": "Vacation",
+            "count": 30
+        }
+    ],
+    "revenueFacts": [
+        {
+            "category": "Outstaffing",
+            "amount": {
+                "amount": 156240,
+                "currency": {
+                    "code": "EUR",
+                    "symbol": "€"
+                }
+            }
+        }
+    ],
+    "lossFacts": [
+        {
+            "category": "Accomodation",
+            "amount": {
+                "amount": 15190,
+                "currency": {
+                    "code": "EUR",
+                    "symbol": "€"
+                }
+            }
+        },
+        {
+            "category": "Fare",
+            "amount": {
+                "amount": 3950,
+                "currency": {
+                    "code": "EUR",
+                    "symbol": "€"
+                }
+            }
+        },
+        {
+            "category": "Royalty",
+            "amount": {
+                "amount": 29999.999999999999999999999934,
+                "currency": {
+                    "code": "EUR",
+                    "symbol": "€"
+                }
+            }
+        },
+        {
+            "category": "Insurance",
+            "amount": {
+                "amount": 17644.998955940697431614115679,
+                "currency": {
+                    "code": "EUR",
+                    "symbol": "€"
+                }
+            }
+        }
+    ],
+    "netProfitFacts": [
+        {
+            "category": "BasicSalary",
+            "amount": {
+                "amount": 49999.999999999999999999999938,
+                "currency": {
+                    "code": "EUR",
+                    "symbol": "€"
+                }
+            }
+        },
+        {
+            "category": "BonusSalaryPlan",
+            "amount": {
+                "amount": 39455.001044059302568385884456,
+                "currency": {
+                    "code": "EUR",
+                    "symbol": "€"
+                }
+            }
+        }
+    ]
+}
